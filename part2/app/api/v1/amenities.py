059ecf132ffc96a1814bdd0b6ad9b94d827ffd76
Lachie <lachie.duncan12@gmail.com>
2025-03-04 02:29:47 +0000

    Added Amenity endpoint

--- part2/app/api/v1/amenities.py
+++ part2/app/api/v1/amenities.py
@@ -11,45 +11,97 @@ amenity_model = api.model('Amenity', {
 @api.route('/')
 class AmenityList(Resource):
     @api.expect(amenity_model)
-    @api.marshal_with(amenity_model)  # decorator
+    # @api.marshal_with(amenity_model)
     @api.response(201, 'Amenity successfully created')
     @api.response(400, 'Invalid input data')
     def post(self):
         """Register a new amenity"""
-        # Placeholder for the logic to register a new amenity
+        
+        # Test this endpoint
+        # curl -X POST http://localhost:5000/api/v1/amenities/ -H "Content-Type: application/json" -d '{"name": "Wi-Fi"}'
+        
         amenity_data = api.payload
-        amenity_created = facade.create_amenity(amenity_data)
-        return amenity_created, 201        
+
+        try:
+            new_amenity = facade.create_amenity(amenity_data)
+            
+            print("id", new_amenity.id)
+
+            return {
+                "id": str(new_amenity.id),
+                "name": new_amenity.name
+            }, 201
+
+        except ValueError:
+            return {'error': 'User data not valid'}, 400    
 
     @api.response(200, 'List of amenities retrieved successfully')
-    @api.marshal_with(amenity_model)  # decorator
+    # @api.marshal_with(amenity_model)
     def get(self):
         """Retrieve a list of all amenities"""
-        # Placeholder for logic to return a list of all amenities
-        amenity_list = facade.get_all_amenities()
-        return amenity_list, 200
+
+        # Test this endpoint
+        # curl -X GET http://localhost:5000/api/v1/amenities/ -H "Content-Type: application/json"
         
+        all_amenities = facade.get_all_amenities()
+        
+        # convert Amenity objects to python dictionaries
+        data = []
+
+        for amenity in all_amenities:
+            data.append({
+                "id": str(amenity.id),
+                "name": amenity.name,
+            })
+
+        return data, 200
+
 
 @api.route('/<amenity_id>')
 class AmenityResource(Resource):
-    @api.marshal_with(amenity_model)  # decorator
+    # @api.marshal_with(amenity_model)
     @api.response(200, 'Amenity details retrieved successfully')
     @api.response(404, 'Amenity not found')
     def get(self, amenity_id):
         """Get amenity details by ID"""
-        # Placeholder for the logic to retrieve an amenity by ID
-        # amenity_data = api.payload
-        amenity_list = facade.get_amenity(amenity_id)
+        
+        # Test this endpoint
+        # curl -X GET http://localhost:5000/api/v1/amenities/<amenity_id> -H "Content-Type: application/json"
 
-        return amenity_list, 200
+        amenity = facade.get_amenity(amenity_id)
 
+        if not amenity:
+            return {'error': 'Amenity not found'}, 404
+
+        return {
+            'id': amenity.id,
+            'name': amenity.name
+        }, 200
 
     @api.expect(amenity_model)
-    @api.marshal_with(amenity_model)  # decorator
+    # @api.marshal_with(amenity_model)
     @api.response(200, 'Amenity updated successfully')
     @api.response(404, 'Amenity not found')
     @api.response(400, 'Invalid input data')
     def put(self, amenity_id):
         """Update an amenity's information"""
-        # Placeholder for the logic to update an amenity by ID
-        pass
+        
+        # Test this endpoint
+        # curl -X POST http://localhost:5000/api/v1/amenities/<amenity_id> -H "Content-Type: application/json" -d '{"name": "Sauna"}'
+        
+        data = api.payload
+        
+        # check if amenity exists
+        amenity = facade.get_amenity(amenity_id)
+
+        if amenity is None:
+            return {'error': 'Amenity not found'}, 404
+
+        try:
+            facade.update_amenity(amenity_id, data)
+
+            return {"message": "Amenity updated successfully"}, 200
+
+        except ValueError:
+            return {'error': 'Invalid input data'}, 400
+            
