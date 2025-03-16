90b579ba570a597bb40e846d05d39be5ed8d8df9
Radew-lab-pixel <ktmqliu@gmail.com>
2025-03-17 02:07:01 +1100

    Add relationship

--- part3_demo/app/models/review.py
+++ part3_demo/app/models/review.py
@@ -1,9 +1,20 @@
 from app.models.base_model import BaseModel
 from app.models.place import Place
 from app.models.user import User
+from sqlalchemy import Column, String, Boolean, Float, Integer
+from sqlalchemy.orm import relationship
 
 class Review(BaseModel):
     """Represents a review of a place by a user in the HBnB Evolution application."""
+    # mapping
+    __tablename__ = 'reviews'
+    
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    __text = Column(String(128), nullable=False)
+    __rating = Column(Integer, nullable=False)
+    user_r = relationship('User', backref='reviews_r')
+    place_r = relationship('Place', backref='reviews_r')
+
     def __init__(self, text, rating, place, user):
         super().__init__()
         if not text or not isinstance(text, str):
