718501ef5518f9568941a6ec5cc7357de10b35f6
Radew-lab-pixel <ktmqliu@gmail.com>
2025-03-24 00:22:40 +1100

    Update hbnb_evo_2_db_202503-23_213200.sql

--- part3_demo/hbnb_evo_2_db_2025-03-23_213200.sql
+++ part3_demo/hbnb_evo_2_db_2025-03-23_213200.sql
@@ -4,18 +4,7 @@
 -- ------------------------------------------------------
 -- Server version	8.0.41-0ubuntu0.20.04.1
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
+-
 -- Table structure for table `amenities`
 --
 
@@ -28,7 +17,8 @@ CREATE TABLE `amenities` (
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `name` varchar(50) NOT NULL,
   `place_id` varchar(36) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (place_id) REFERENCES places(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -49,7 +39,8 @@ CREATE TABLE `places` (
   `latitude` float NOT NULL,
   `longitude` float NOT NULL,
   `owner_id` varchar(60) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (owner_id) REFERENCES users(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -68,7 +59,9 @@ CREATE TABLE `reviews` (
   `rating` int NOT NULL,
   `place_id` varchar(60) NOT NULL,
   `user_id` varchar(60) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (place_id) REFERENCES places(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -91,19 +84,14 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping routines for database 'hbnb_evo_2_db'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO users (
+  id, created_at, updated_at, first_name, last_name, email, password, is_admin)
+  VALUES ('36c9050e-ddd3-4c3b-9731-9f487208bbc1',
+  CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'Admin', 'HBnB', 'admin@hbnb.io', 'admin1234', TRUE);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-03-23 21:32:16
+INSERT INTO amenities (
+  id, created_at, updated_at, name, place_id)
+  VALUES (UUID(), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP,'WIFI',''),
+  (UUID(), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'Swimming Pool', ''),
+  (UUID(), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'Air-Conditioning','');
