c4852ff115d9219c231731786b3abd8bc8478c60
chak8x8 <chak8x8@gmail.com>
2025-03-23 11:31:14 +1100

    Task 9: Add schema.sql and seed.sql for database schema and initial data

---
+++ part3/seed.sql
@@ -0,0 +1,27 @@
+INSERT INTO users (id, first_name, last_name, email, password, is_admin)
+VALUES (
+    '36c9050e-ddd3-4c3b-9731-9f487208bbc1',
+    'Admin',
+    'HBnB',
+    'admin@hbnb.io',
+    '$2b$12$yourhashedpasswordhere',
+    TRUE
+);
+
+INSERT INTO amenities (id, name)
+VALUES (
+    '14da5756-110d-4072-81c5-b091ba8d1f4e',
+    'WiFi'
+);
+
+INSERT INTO amenities (id, name)
+VALUES (
+    '513c6d29-99b1-4f4d-b1d9-ab1c1743073c',
+    'Swimming Pool'
+);
+
+INSERT INTO amenities (id, name)
+VALUES (
+    '2bddb9b0-d45d-4e22-ae8a-7c687f5625ab',
+    'Air Conditioning'
+);
