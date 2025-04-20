e4576887141558f5d1c50dd26e3bca26b110bd2e
Radew-lab-pixel <ktmqliu@gmail.com>
2025-04-21 00:01:25 +1000

    Issues with JWT but index.html ok wiht python3 -m http.server 5500

--- part4/frontend/base_files/scripts.js
+++ part4/frontend/base_files/scripts.js
@@ -41,7 +41,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 async function loginUser(email, password) {
-  const response = await fetch('http://127.0.0.1:5500/api/v1/auth/login', {
+  //const response = await fetch('http://127.0.0.1:5500/api/v1/auth/login', {
+  const response = await fetch('http://127.0.0.1:5000/api/v1/auth/login', {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json'
